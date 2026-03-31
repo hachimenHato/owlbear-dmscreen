64e44bf970f7beef2141b2adfe95713008e74537
zhanghao33 <zhanghao33@yusys.com.cn>
2026-03-31 10:47:29 +0800

    4

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   // 核心：这里填你的GitHub仓库名，和实际完全一致（大小写敏感）
-  base: '/owlbear-dmscreen/',
+  base: '/',
   // 下面的原有配置（plugins等）保持不变
   server: {
     cors: {
